43bae2382bc3ea0ca711bddc9012f6859c552590
denlyckligakompisen <frebrandberg@gmail.com>
2026-05-30 23:01:08 +0200

    feat: implement App routing and base styling tokens

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,17 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import VMKollen from './components/VMKollen';
 import AllsvenskanKollen from './components/AllsvenskanKollen';
 
-import PullToRefresh from './components/common/PullToRefresh';
-
 function App() {
   return (
-    <PullToRefresh>
-        <div className="app-container">
-          <main>
-            <Routes>
-              <Route path="/vm" element={<VMKollen />} />
-              <Route path="/allsvenskan" element={<AllsvenskanKollen />} />
-              <Route path="/" element={<Navigate to="/vm" replace />} />
-            </Routes>
-          </main>
-        </div>
-    </PullToRefresh>
+    <div className="app-container">
+      <main>
+        <Routes>
+          <Route path="/vm" element={<VMKollen />} />
+          <Route path="/allsvenskan" element={<AllsvenskanKollen />} />
+          <Route path="/" element={<Navigate to="/vm" replace />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
